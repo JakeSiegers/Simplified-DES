b99242e8703e99e305fb1353e67fc34467d2ca5c
Jake Siegers <sirtopeia@yahoo.com>
2014-02-27 18:23:03 -0600

    Converted To BitSet - Got a bug to fix though.

--- src/DESDriver.java
+++ src/DESDriver.java
@@ -1,7 +1,30 @@
+import sun.net.www.content.text.plain;
+
+import java.util.BitSet;
+
 public class DESDriver {
     public static void main(String[] args){
 
         try {
+            BitSet plaintext = strToBitSet("100010110101");
+            BitSet key = strToBitSet("111000111");
+            BitSet ciphertext;
+
+            SimplifiedDES desObj = new SimplifiedDES(true);
+
+            pl(plaintext,12);
+            pl(key,9);
+            ciphertext = desObj.encrypt(plaintext,key,6);
+            pl(ciphertext,12);
+            pl(desObj.decrypt(ciphertext,key,6),12);
+
+            //plaintext.xor(key);
+
+            //pl(plaintext,7);
+
+            System.exit(0);
+
+       /*
             SimplifiedDES desObj = new SimplifiedDES(false);
 
             int[] inta = {1,0,0,0,1,0,1,1,0,1,0,1};
@@ -19,6 +42,7 @@ public class DESDriver {
             pl(desObj.toString(plaintext));
             pl(desObj.toString(ciphertext));
             pl(desObj.toString(plaintext2));
+            */
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -26,7 +50,23 @@ public class DESDriver {
         }
     }
 
-    private static void pl(Object x){
-        System.out.println(x);
+    private static BitSet strToBitSet(String in) {
+        BitSet out = new BitSet(in.length());
+        for(int i=0;i<in.length();i++){
+            out.set(i,(in.charAt(i) == '1'));
+        }
+        return out;
+    }
+
+    private static String bitSetToStr(BitSet in,int bytes) {
+        String out="";
+        for(int i=0;i<bytes;i++){
+            out=out+(in.get(i)?"1":"0");
+        }
+        return out;
+    }
+
+    private static void pl(BitSet x,int bytes){
+        System.out.println(bitSetToStr(x,bytes));
     }
 }
--- src/SimplifiedDES.java
+++ src/SimplifiedDES.java
@@ -1,3 +1,5 @@
+import java.util.BitSet;
+
 public class SimplifiedDES{
     boolean enableDebug;
 
@@ -6,159 +8,156 @@ public class SimplifiedDES{
     }
 
     //12-bit ciphertext  encrypt(12-bit plantext,9-bit key,number of rounds)
-    public boolean[] encrypt(boolean[] plaintext, boolean[] key, int rounds) throws Exception {
+    public BitSet encrypt(BitSet plaintext, BitSet key, int rounds) throws Exception {
        pl("===== Starting Encrypt =====");
 
         //2-d array for L & R where 1st dimension is round and 2nd is the current array of values
-        boolean[][] L = new boolean[rounds+1][6];
-        boolean[][] R = new boolean[rounds+1][6];
-        boolean[][] K = new boolean[rounds+1][9];
+        BitSet[] L = new BitSet[rounds+1];
+        BitSet[] R = new BitSet[rounds+1];
+        BitSet[] K = new BitSet[rounds+1];
 
         //Setting up round 0 for vars
-        boolean[][] splitPlainText = splitArray(plaintext);
+        BitSet[] splitPlainText = splitArray(plaintext,12);
         L[0]=splitPlainText[0];
         R[0]=splitPlainText[1];
         p("encrypt L[0]: ");
-        pl(L[0]);
+        pl(L[0],6);
         p("encrypt R[0]: ");
-        pl(R[0]);
-        p("decrypt key: ");
-        pl(key);
+        pl(R[0],6);
+        p("encrypt key: ");
+        pl(key,9);
 
         //loop start
         int curRound;
         for(curRound=1;curRound<=rounds;curRound++){
             K[curRound] = roundKey(key,curRound); //Round key.
             p("encrypt K["+curRound+"]: ");
-            pl(K[curRound]);
+            pl(K[curRound],8);
 
             p("encrypt r["+curRound+"]: ");
-            boolean[][] LR = splitArray(simpleDES(joinArrays(L[curRound-1],R[curRound-1]),K[curRound]));
+            BitSet[] LR = splitArray(simpleDES(joinArrays(L[curRound-1],R[curRound-1],6),K[curRound]),12);
             L[curRound] = LR[0];
             R[curRound] = LR[1];
 
             p("encrypt L["+curRound+"]: ");
-            pl(L[curRound]);
+            pl(L[curRound],6);
             p("encrypt R["+curRound+"]: ");
-            pl(R[curRound]);
+            pl(R[curRound],6);
         }
         curRound--;
-        return joinArrays(R[curRound],L[curRound]);
+        return joinArrays(R[curRound],L[curRound],6);
     }
 
     //Returns a 2d array containing each half of the in array.
-    public boolean[][] splitArray(boolean[] in) throws Exception {
-        int inLen=in.length;
+    public BitSet[] splitArray(BitSet in,int length) throws Exception {
+        int inLen=length;
         int half_inLen = inLen/2;
         if(inLen%2 != 0){
             throw new Exception("The splitArray function can only handle even length arrays. (odd arrays should never happen in this problem!)");
         }
-        boolean[][] out = new boolean[2][half_inLen];
-        for(int x=0;x<half_inLen;x++){
-            out[0][x] = in[x];
-            out[1][half_inLen-1-x] = in[inLen-1-x];
-        }
+        BitSet[] out = new BitSet[2];
+        out[0]=in.get(0,half_inLen-3);
+        out[1]=in.get(half_inLen,inLen-1);
         return out;
     }
 
-    public boolean[] decrypt(boolean[] plaintext, boolean[] key, int rounds) throws Exception {
+    public BitSet decrypt(BitSet plaintext, BitSet key, int rounds) throws Exception {
         pl("===== Starting Decrypt =====");
 
         //2-d array for L & R where 1st dimension is round and 2nd is the current array of values
-        boolean[][] L = new boolean[rounds+2][6]; //rounds+1 is the starting index for variables.
-        boolean[][] R = new boolean[rounds+2][6];
-        boolean[][] K = new boolean[rounds+2][9];
+        BitSet[] L = new BitSet[rounds+2]; //rounds+1 is the starting index for variables.
+        BitSet[] R = new BitSet[rounds+2];
+        BitSet[] K = new BitSet[rounds+2];
 
         //Setting up round 0 for vars
-        boolean[][] splitPlainText = splitArray(plaintext);
+        BitSet[] splitPlainText = splitArray(plaintext,12);
         L[rounds+1]=splitPlainText[0];
         R[rounds+1]=splitPlainText[1];
         p("decrypt L["+(rounds+1)+"]: ");
-        pl(L[rounds+1]);
+        pl(L[rounds+1],6);
         p("decrypt R["+(rounds+1)+"]: ");
-        pl(R[rounds+1]);
+        pl(R[rounds+1],6);
         p("decrypt key: ");
-        pl(key);
+        pl(key,9);
 
         //loop start
         int curRound;
         for(curRound=rounds;curRound>0;curRound--){
             K[curRound] = roundKey(key,curRound); //Round key.
             p("decrypt K["+curRound+"]: ");
-            pl(K[curRound]);
+            pl(K[curRound],8);
 
             p("decrypt r["+curRound+"]: ");
-            boolean[][] LR = splitArray(simpleDES(joinArrays(L[curRound+1],R[curRound+1]),K[curRound]));
+            BitSet[] LR = splitArray(simpleDES(joinArrays(L[curRound+1],R[curRound+1],6),K[curRound]),12);
             L[curRound] = LR[0];
             R[curRound] = LR[1];
 
             p("decrypt L["+curRound+"]: ");
-            pl(L[curRound]);
+            pl(L[curRound],6);
             p("decrypt R["+curRound+"]: ");
-            pl(R[curRound]);
+            pl(R[curRound],6);
         }
         curRound++;
-        return joinArrays(R[curRound],L[curRound]);
+        return joinArrays(R[curRound],L[curRound],6);
     }
 
-    //12 bit input and 8-bit round key.
-    public boolean[] simpleDES(boolean[] LR, boolean[] K) throws Exception {
-        boolean[] outL;
-        boolean[] outR;
-        boolean[][] LRSplit = splitArray(LR);
-        boolean[] L =LRSplit[0];
-        boolean[] R =LRSplit[1];
-        boolean[] roundResult = roundFunction(R, K);
-        pl(roundResult);
+    //12 bit input and 8-bit round key. returns a 12-bit
+    public BitSet simpleDES(BitSet LR, BitSet K) throws Exception {
+        BitSet outL;
+        BitSet outR;
+        BitSet[] LRSplit = splitArray(LR,12);
+        BitSet L =LRSplit[0];
+        BitSet R =LRSplit[1];
+        BitSet roundResult = roundFunction(R, K);
+        pl(roundResult,6);
         outL = R;
         outR = xorArrays(L,roundResult);
-        return joinArrays(outL,outR);
+        return joinArrays(outL,outR,6);
+    }
+
+    public BitSet xorArrays(BitSet a,BitSet b){
+        BitSet out;
+        out=(BitSet)a.clone();
+        out.xor(b);
+        return out;
     }
 
     //Takes a 6-bit R and using an 8-bit K, creates a 6-bit return
-    public boolean[] roundFunction(boolean[] R,boolean[] K) throws Exception {
-        if(R.length != 6){
-            throw new Exception("R in roundFunction() is not a 6-bit value!");
-        }
-        if(K.length != 8){
-            throw new Exception("K in roundFunction() is not an 8-bit value!");
-        }
-        boolean[] ER = expander(R);
-        boolean[] xorER = xorArrays(ER,K);
-        boolean[][] splitXorER = splitArray(xorER);
-        boolean[] sboxd1 = sbox1(splitXorER[0]);
-        boolean[] sboxd2 = sbox2(splitXorER[1]);
-        boolean[] out = joinArrays(sboxd1,sboxd2);
-        if(out.length != 6){
-            throw new Exception("roundFunction() did not return a 6-bit value!");
-        }
+    public BitSet roundFunction(BitSet R,BitSet K) throws Exception {
+
+        BitSet ER = expander(R);
+        BitSet xorER = xorArrays(ER,K);
+        BitSet[] splitXorER = splitArray(xorER,8);
+        BitSet sboxd1 = sbox1(splitXorER[0]);
+        BitSet sboxd2 = sbox2(splitXorER[1]);
+        BitSet out = joinArrays(sboxd1,sboxd2,3);
         return out;
     }
 
     // 8-bit output expander(6-bit input)
-    public boolean[] expander(boolean[] in){
-        boolean[] out = new boolean[8];
+    public BitSet expander(BitSet in){
+        BitSet out = new BitSet(8);
         for(int i=0;i<6;i++){
             switch(i){
                 case 0:
-                    out[0]=in[i];
+                    out.set(0,in.get(i));
                     break;
                 case 1:
-                    out[1]=in[i];
+                    out.set(1,in.get(i));
                     break;
                 case 2:
-                    out[3]=in[i];
-                    out[5]=in[i];
+                    out.set(3,in.get(i));
+                    out.set(5,in.get(i));
                     break;
                 case 3:
-                    out[2]=in[i];
-                    out[4]=in[i];
+                    out.set(2,in.get(i));
+                    out.set(4,in.get(i));
                     break;
                 case 4:
-                    out[6]=in[i];
+                    out.set(6,in.get(i));
                     break;
                 case 5:
-                    out[7]=in[i];
+                    out.set(7,in.get(i));
                     break;
             }
         }
@@ -166,113 +165,82 @@ public class SimplifiedDES{
     }
 
     //Index 0 is Row, Index 1 is Col
-    private int[] determineRowColForSboxes(boolean[] in) throws Exception {
-        if(in.length!=4){
-            throw new Exception("I expect a 4-bit value in determineRowColForSboxes()");
-        }
+    private int[] determineRowColForSboxes(BitSet in){
         int[] out = new int[2];
-        out[0] = (in[0])?1:0; //row
+        out[0] = in.get(0)?1:0; //row
         boolean[] colArray = new boolean[3];
         System.arraycopy(in, 1, colArray, 0, 3);
         out[1] = Integer.parseInt(toString(colArray),2); //col
         return out;
     }
 
-    public boolean[] sbox1(boolean[] in) throws Exception {
-        boolean[][][] s1 = {
+    public BitSet sbox1(BitSet in){
+        String[][] s1 = {
             {
-                {true,false,true}
-                ,{false,true,false}
-                ,{false,false,true}
-                ,{true,true,false}
-                ,{false,true,true}
-                ,{true,false,false}
-                ,{true,true,true}
-                ,{false,false,false}
+                 "101"
+                ,"010"
+                ,"001"
+                ,"110"
+                ,"011"
+                ,"100"
+                ,"111"
+                ,"000"
             }
             ,{
-                {false,false,true}
-                ,{true,false,false}
-                ,{true,true,false}
-                ,{false,true,false}
-                ,{false,false,false}
-                ,{true,true,true}
-                ,{true,false,true}
-                ,{false,true,true}
+                 "001"
+                ,"100"
+                ,"110"
+                ,"010"
+                ,"000"
+                ,"111"
+                ,"101"
+                ,"011"
              }
         };
         int[] rowCol = determineRowColForSboxes(in);
-        return s1[rowCol[0]][rowCol[1]];
+        return strToBitSet(s1[rowCol[0]][rowCol[1]]);
     }
 
-    public boolean[] sbox2(boolean[] in) throws Exception {
-        boolean[][][] s2 = {
+    public BitSet sbox2(BitSet in){
+        String[][] s2 = {
             {
-                {true,false,false}
-                ,{false,false,false}
-                ,{true,true,false}
-                ,{true,false,true}
-                ,{true,true,true}
-                ,{false,false,true}
-                ,{false,true,true}
-                ,{false,true,false}
+                 "100"
+                ,"000"
+                ,"110"
+                ,"101"
+                ,"111"
+                ,"001"
+                ,"011"
+                ,"010"
             }
             ,{
-                {true,false,true}
-                ,{false,true,true}
-                ,{false,false,false}
-                ,{true,true,true}
-                ,{true,true,false}
-                ,{false,true,false}
-                ,{false,false,true}
-                ,{true,false,false}
+                "101"
+                ,"011"
+                ,"000"
+                ,"111"
+                ,"110"
+                ,"010"
+                ,"001"
+                ,"100"
             }
         };
         int[] rowCol = determineRowColForSboxes(in);
-        return s2[rowCol[0]][rowCol[1]];
+        return strToBitSet(s2[rowCol[0]][rowCol[1]]);
     }
 
     //Takes 9-bit Key K and round i and returns a 8-bit round key.
-    public boolean[] roundKey(boolean[] K, int i) throws Exception {
-        if(K.length != 9){
-            throw new Exception("K in roundKey() is not a 9-bit value!");
-        }
-        boolean[] twoK = joinArrays(K,K);
-        boolean[] out = new boolean[8];
+    public BitSet roundKey(BitSet K, int i) {
+        String twoK = bitSetToStr(K,9)+bitSetToStr(K,9);
+        BitSet out = new BitSet(8);
         int startPt = (i-1)%9;
         for(int l=0,j=startPt;j<startPt+8;j++,l++){
-            out[l]=twoK[j];
+            out.set(l,twoK.charAt(j) == '1');
         }
         return out;
     }
 
-    public boolean[] xorArrays(boolean [] a,boolean[] b) throws Exception {
-        int alen = a.length;
-        int blen = b.length;
-        if(alen!=blen){
-           throw new Exception("I want the arrays in xorArrays() to be the same length!");
-        }
-        boolean[] output = new boolean[alen];
-        for(int i=0;i<alen;i++){
-           output[i] = a[i] ^ b[i];
-        }
-        return output;
-    }
-
-    public boolean[] joinArrays(boolean[] a,boolean[] b) throws Exception {
-        int alen = a.length;
-        int blen = b.length;
-        //Joining arrays should always be the same length in this problem.
-        if(alen!=blen){
-            throw new Exception("I want the arrays in joinArrays() to be the same length!");
-        }
-        int ablen = alen+blen;
-        boolean[] out = new boolean[ablen];
-        for(int i=0;i<alen;i++){
-            out[i]=a[i];
-            out[i+alen]=b[i];
-        }
-        return out;
+    public BitSet joinArrays(BitSet a,BitSet b, int eachSize){
+        return strToBitSet(bitSetToStr(a,eachSize)+bitSetToStr(b,eachSize));
     }
 
     public String toString(boolean[] a){
@@ -294,6 +262,26 @@ public class SimplifiedDES{
             output[i] = a[i] == 1;
         }
         return output;
+    }
+
+    private BitSet strToBitSet(String in) {
+        BitSet out = new BitSet(in.length());
+        for(int i=0;i<in.length();i++){
+            out.set(i,(in.charAt(i) == '1'));
+        }
+        return out;
+    }
+
+    private String bitSetToStr(BitSet in,int bytes) {
+        String out="";
+        for(int i=0;i<bytes;i++){
+            out=out+(in.get(i)?"1":"0");
+        }
+        return out;
+    }
+
+    private void pl(BitSet x,int bytes){
+        System.out.println(bitSetToStr(x,bytes));
     }
 
     private void p(String x){
