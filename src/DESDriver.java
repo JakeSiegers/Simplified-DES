9668e6820d064a758822f0d03cc544d9e3a536e8
Jake Siegers <sirtopeia@yahoo.com>
2014-03-13 12:55:59 -0500

    Rolled Back to Boolean arrays. Bitset objects were wrong - ouch. Also stripped out debug and exception throwing - professor didn't like that at all.

--- src/DESDriver.java
+++ src/DESDriver.java
@@ -1,29 +1,11 @@
 public class DESDriver {
     public static void main(String[] args){
 
-        try {
-            SimplifiedDES desObj = new SimplifiedDES(false);
+        boolean[] x = {false,false,false,false,false,false,false,false,false,false,false,false};
+        boolean[] y = {false,false,false,false,false,false,false,false};
+        //for(int i =0;i<=10;i++)
+        pl(SimplifiedDES.toString(SimplifiedDES.decrypt(x,y,128)));
 
-            int[] inta = {1,0,0,0,1,0,1,1,0,1,0,1};
-            int[] intKey = {1,1,1,0,0,0,1,1,1};
-
-            boolean[] plaintext = desObj.intArrayToBoolArray(inta);
-            boolean[] key = desObj.intArrayToBoolArray(intKey);
-
-            boolean[] ciphertext = desObj.encrypt(plaintext, key, 6);
-
-            key = desObj.intArrayToBoolArray(intKey);
-
-            boolean[] plaintext2 = desObj.decrypt(ciphertext, key,6);
-
-            pl(desObj.toString(plaintext));
-            pl(desObj.toString(ciphertext));
-            pl(desObj.toString(plaintext2));
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
     }
 
     private static void pl(Object x){
--- src/SimplifiedDES.java
+++ src/SimplifiedDES.java
@@ -1,14 +1,13 @@
+/*
+Jake Siegers
+Assignment 2
+3/13/14
+
+This is my simpleDES implementation using Boolean Arrays.
+ */
+
 public class SimplifiedDES{
-    boolean enableDebug;
-
-    SimplifiedDES(boolean enableDebug){
-        this.enableDebug=enableDebug;
-    }
-
-    //12-bit ciphertext  encrypt(12-bit plantext,9-bit key,number of rounds)
-    public boolean[] encrypt(boolean[] plaintext, boolean[] key, int rounds) throws Exception {
-       pl("===== Starting Encrypt =====");
-
+    public static boolean[] encrypt(boolean[] plaintext, boolean[] key, int rounds){
         //2-d array for L & R where 1st dimension is round and 2nd is the current array of values
         boolean[][] L = new boolean[rounds+1][6];
         boolean[][] R = new boolean[rounds+1][6];
@@ -18,41 +17,23 @@ public class SimplifiedDES{
         boolean[][] splitPlainText = splitArray(plaintext);
         L[0]=splitPlainText[0];
         R[0]=splitPlainText[1];
-        p("encrypt L[0]: ");
-        pl(L[0]);
-        p("encrypt R[0]: ");
-        pl(R[0]);
-        p("decrypt key: ");
-        pl(key);
 
         //loop start
         int curRound;
         for(curRound=1;curRound<=rounds;curRound++){
             K[curRound] = roundKey(key,curRound); //Round key.
-            p("encrypt K["+curRound+"]: ");
-            pl(K[curRound]);
-
-            p("encrypt r["+curRound+"]: ");
             boolean[][] LR = splitArray(simpleDES(joinArrays(L[curRound-1],R[curRound-1]),K[curRound]));
             L[curRound] = LR[0];
             R[curRound] = LR[1];
-
-            p("encrypt L["+curRound+"]: ");
-            pl(L[curRound]);
-            p("encrypt R["+curRound+"]: ");
-            pl(R[curRound]);
         }
         curRound--;
         return joinArrays(R[curRound],L[curRound]);
     }
 
     //Returns a 2d array containing each half of the in array.
-    public boolean[][] splitArray(boolean[] in) throws Exception {
+    public static boolean[][] splitArray(boolean[] in){
         int inLen=in.length;
         int half_inLen = inLen/2;
-        if(inLen%2 != 0){
-            throw new Exception("The splitArray function can only handle even length arrays. (odd arrays should never happen in this problem!)");
-        }
         boolean[][] out = new boolean[2][half_inLen];
         for(int x=0;x<half_inLen;x++){
             out[0][x] = in[x];
@@ -61,8 +42,7 @@ public class SimplifiedDES{
         return out;
     }
 
-    public boolean[] decrypt(boolean[] plaintext, boolean[] key, int rounds) throws Exception {
-        pl("===== Starting Decrypt =====");
+    public static boolean[] decrypt(boolean[] plaintext, boolean[] key, int rounds){
 
         //2-d array for L & R where 1st dimension is round and 2nd is the current array of values
         boolean[][] L = new boolean[rounds+2][6]; //rounds+1 is the starting index for variables.
@@ -73,70 +53,45 @@ public class SimplifiedDES{
         boolean[][] splitPlainText = splitArray(plaintext);
         L[rounds+1]=splitPlainText[0];
         R[rounds+1]=splitPlainText[1];
-        p("decrypt L["+(rounds+1)+"]: ");
-        pl(L[rounds+1]);
-        p("decrypt R["+(rounds+1)+"]: ");
-        pl(R[rounds+1]);
-        p("decrypt key: ");
-        pl(key);
 
         //loop start
         int curRound;
         for(curRound=rounds;curRound>0;curRound--){
             K[curRound] = roundKey(key,curRound); //Round key.
-            p("decrypt K["+curRound+"]: ");
-            pl(K[curRound]);
-
-            p("decrypt r["+curRound+"]: ");
             boolean[][] LR = splitArray(simpleDES(joinArrays(L[curRound+1],R[curRound+1]),K[curRound]));
             L[curRound] = LR[0];
             R[curRound] = LR[1];
-
-            p("decrypt L["+curRound+"]: ");
-            pl(L[curRound]);
-            p("decrypt R["+curRound+"]: ");
-            pl(R[curRound]);
         }
         curRound++;
         return joinArrays(R[curRound],L[curRound]);
     }
 
     //12 bit input and 8-bit round key.
-    public boolean[] simpleDES(boolean[] LR, boolean[] K) throws Exception {
+    public static boolean[] simpleDES(boolean[] LR, boolean[] K){
         boolean[] outL;
         boolean[] outR;
         boolean[][] LRSplit = splitArray(LR);
         boolean[] L =LRSplit[0];
         boolean[] R =LRSplit[1];
         boolean[] roundResult = roundFunction(R, K);
-        pl(roundResult);
         outL = R;
         outR = xorArrays(L,roundResult);
         return joinArrays(outL,outR);
     }
 
     //Takes a 6-bit R and using an 8-bit K, creates a 6-bit return
-    public boolean[] roundFunction(boolean[] R,boolean[] K) throws Exception {
-        if(R.length != 6){
-            throw new Exception("R in roundFunction() is not a 6-bit value!");
-        }
-        if(K.length != 8){
-            throw new Exception("K in roundFunction() is not an 8-bit value!");
-        }
+    public static boolean[] roundFunction(boolean[] R,boolean[] K){
         boolean[] ER = expander(R);
         boolean[] xorER = xorArrays(ER,K);
         boolean[][] splitXorER = splitArray(xorER);
         boolean[] sboxd1 = sbox1(splitXorER[0]);
         boolean[] sboxd2 = sbox2(splitXorER[1]);
         boolean[] out = joinArrays(sboxd1,sboxd2);
-        if(out.length != 6){
-            throw new Exception("roundFunction() did not return a 6-bit value!");
-        }
         return out;
     }
 
     // 8-bit output expander(6-bit input)
-    public boolean[] expander(boolean[] in){
+    public static boolean[] expander(boolean[] in){
         boolean[] out = new boolean[8];
         for(int i=0;i<6;i++){
             switch(i){
@@ -166,10 +121,7 @@ public class SimplifiedDES{
     }
 
     //Index 0 is Row, Index 1 is Col
-    private int[] determineRowColForSboxes(boolean[] in) throws Exception {
-        if(in.length!=4){
-            throw new Exception("I expect a 4-bit value in determineRowColForSboxes()");
-        }
+    private static int[] determineRowColForSboxes(boolean[] in){
         int[] out = new int[2];
         out[0] = (in[0])?1:0; //row
         boolean[] colArray = new boolean[3];
@@ -178,7 +130,7 @@ public class SimplifiedDES{
         return out;
     }
 
-    public boolean[] sbox1(boolean[] in) throws Exception {
+    public static boolean[] sbox1(boolean[] in){
         boolean[][][] s1 = {
             {
                 {true,false,true}
@@ -205,7 +157,7 @@ public class SimplifiedDES{
         return s1[rowCol[0]][rowCol[1]];
     }
 
-    public boolean[] sbox2(boolean[] in) throws Exception {
+    public static boolean[] sbox2(boolean[] in){
         boolean[][][] s2 = {
             {
                 {true,false,false}
@@ -233,25 +185,19 @@ public class SimplifiedDES{
     }
 
     //Takes 9-bit Key K and round i and returns a 8-bit round key.
-    public boolean[] roundKey(boolean[] K, int i) throws Exception {
-        if(K.length != 9){
-            throw new Exception("K in roundKey() is not a 9-bit value!");
-        }
+    public static boolean[] roundKey(boolean[] K, int i){
+
         boolean[] twoK = joinArrays(K,K);
         boolean[] out = new boolean[8];
-        int startPt = (i-1)%9;
+        int startPt = (i)%9;
         for(int l=0,j=startPt;j<startPt+8;j++,l++){
             out[l]=twoK[j];
         }
         return out;
     }
 
-    public boolean[] xorArrays(boolean [] a,boolean[] b) throws Exception {
+    public static boolean[] xorArrays(boolean [] a,boolean[] b){
         int alen = a.length;
-        int blen = b.length;
-        if(alen!=blen){
-           throw new Exception("I want the arrays in xorArrays() to be the same length!");
-        }
         boolean[] output = new boolean[alen];
         for(int i=0;i<alen;i++){
            output[i] = a[i] ^ b[i];
@@ -259,13 +205,9 @@ public class SimplifiedDES{
         return output;
     }
 
-    public boolean[] joinArrays(boolean[] a,boolean[] b) throws Exception {
+    public static boolean[] joinArrays(boolean[] a,boolean[] b){
         int alen = a.length;
         int blen = b.length;
-        //Joining arrays should always be the same length in this problem.
-        if(alen!=blen){
-            throw new Exception("I want the arrays in joinArrays() to be the same length!");
-        }
         int ablen = alen+blen;
         boolean[] out = new boolean[ablen];
         for(int i=0;i<alen;i++){
@@ -275,7 +217,7 @@ public class SimplifiedDES{
         return out;
     }
 
-    public String toString(boolean[] a){
+    public static String toString(boolean[] a){
         String output = "";
         for (boolean aBoolVal : a) {
             if (aBoolVal) {
@@ -285,32 +227,5 @@ public class SimplifiedDES{
             }
         }
         return output;
-    }
-
-    public boolean[] intArrayToBoolArray(int[] a){
-        int alen = a.length;
-        boolean[] output = new boolean[alen];
-        for(int i=0;i<alen;i++){
-            output[i] = a[i] == 1;
-        }
-        return output;
-    }
-
-    private void p(String x){
-        if(enableDebug){
-            System.out.print(x);
-        }
-    }
-
-    private void pl(String x){
-        if(enableDebug){
-            System.out.println(x);
-        }
-    }
-
-    private void pl(boolean[] x){
-        if(enableDebug){
-            System.out.println(toString(x));
-        }
     }
 }
